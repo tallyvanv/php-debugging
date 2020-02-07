8387b59e8e17599d48a22983e245be6280f67a8d
tallyvanv <talisa.vdv@gmail.com>
2020-02-07 12:01:34 +0100

    got to ex 4

--- expert.php
+++ expert.php
@@ -1,2 +1,33 @@
 <?php
 declare(strict_types=1);
+
+echo "Exercise 1 starts here:";
+
+function new_exercise($x) {
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+}
+
+new_exercise(2);
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
+
+
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str = 'Debugged ! Also very fun';
+echo substr($str, 0, 10);
+
+new_exercise(4);
+
+foreach($week as &$day) {
+    $day = substr($day, 0, strlen($day)-3);
+}
+
+print_r($week);
+
